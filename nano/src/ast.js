4f97ba1056f7d4605c423ad14d60765f9a016716
Jason Barr <jason.barr@metronetinc.com>
2022-05-14 17:43:58 -0500

    Define ast nodes

---
+++ nano/src/ast.js
@@ -0,0 +1,12 @@
+import { createType, VariantInfo } from "@jasonsbarr/functional-core";
+
+const variantInfos = [
+  VariantInfo("IntegerLiteral", ["value"]),
+  VariantInfo("BooleanLiteral", ["value"]),
+  VariantInfo("Identifier", ["value"]),
+  VariantInfo("BinaryOperation", ["left", "operator", "right"]),
+  VariantInfo("UnaryOperation", ["operator", "operand"]),
+  VariantInfo("LetExpression", "assign", "body"),
+];
+
+export const Ast = createType("Ast", variantInfos);
