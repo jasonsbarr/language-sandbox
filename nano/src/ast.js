af299b00a2167c41e08b3586881924461fa6a613
Jason Barr <jason.barr@metronetinc.com>
2022-05-14 17:59:40 -0500

    Change prop to name to make more sense

--- nano/src/ast.js
+++ nano/src/ast.js
@@ -3,7 +3,7 @@ import { createType, VariantInfo } from "@jasonsbarr/functional-core";
 const variantInfos = [
   VariantInfo("IntegerLiteral", ["value"]),
   VariantInfo("BooleanLiteral", ["value"]),
-  VariantInfo("Identifier", ["value"]),
+  VariantInfo("Identifier", ["name"]),
   VariantInfo("BinaryOperation", ["left", "operator", "right"]),
   VariantInfo("UnaryOperation", ["operator", "operand"]),
   VariantInfo("LetExpression", "assign", "body"),
